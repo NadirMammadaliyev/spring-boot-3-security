18a1fb12661c55bc21dfef92b40bf15ebdd70f36
Nadir Mammadaliyev <nadirmammadaliyev@KapMac160-M.local>
2023-03-11 21:46:25 +0400

    config swagger

--- src/main/java/az/nadir/springsecurity/config/SecurityConfiguration.java
+++ src/main/java/az/nadir/springsecurity/config/SecurityConfiguration.java
@@ -26,7 +26,18 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
+                .requestMatchers("/api/v1/auth/**",
+                        // -- Swagger UI v2
+                        "/v2/api-docs",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        // -- Swagger UI v3 (OpenAPI)
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
